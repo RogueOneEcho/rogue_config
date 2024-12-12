961adc3da373b2143f7a113f7cc18a278c348a92
RogueOneEcho <um9ndwvpbmvfy2hv@pm.me>
2024-12-12 23:14:43 +0000

    feat: initial commit

---
+++ src/example.rs
@@ -0,0 +1,49 @@
+use crate::example::Enumerable::One;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::path::PathBuf;
+
+#[derive(Debug, Deserialize, PartialEq, Serialize)]
+pub struct Example {
+    string: String,
+    path: PathBuf,
+    isize: isize,
+    usize: usize,
+    bool: bool,
+    f32: f32,
+    vec: Vec<String>,
+    hash_map: HashMap<String, usize>,
+    option: Option<usize>,
+    tuple: (String, usize),
+    enumerable: Enumerable,
+}
+
+#[derive(Debug, Deserialize, PartialEq, Serialize)]
+pub enum Enumerable {
+    Zero,
+    One,
+    Two,
+}
+
+impl Example {
+    #[allow(clippy::self_named_constructors)]
+    pub fn example() -> Example {
+        Example {
+            string: "Hello, world!".to_owned(),
+            path: PathBuf::from("/path/to/file"),
+            isize: -12345,
+            usize: 12345,
+            f32: -12_345.679,
+            bool: true,
+            vec: vec!["One".to_owned(), "Two".to_owned(), "Three".to_owned()],
+            hash_map: HashMap::from([
+                ("one".to_owned(), 1),
+                ("two".to_owned(), 2),
+                ("three".to_owned(), 3),
+            ]),
+            option: Some(12345),
+            tuple: ("one".to_owned(), 1),
+            enumerable: One,
+        }
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+pub use provider::*;
+pub use yaml::*;
+#[cfg(test)]
+mod example;
+mod provider;
+mod yaml;
